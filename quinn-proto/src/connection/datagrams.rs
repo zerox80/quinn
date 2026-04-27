0ab12c79515144e871211d0bc2ec797675aea7b2
zerox80 <rujbin@proton.me>
2026-04-27 22:55:55 +0200

    fix: handle early 1-RTT packets, packet limits and socket rebinding

--- quinn-proto/src/connection/datagrams.rs
+++ quinn-proto/src/connection/datagrams.rs
@@ -77,6 +77,10 @@ impl Datagrams<'_> {
     ///
     /// Not necessarily the maximum size of received datagrams.
     pub fn max_size(&self) -> Option<usize> {
+        if self.conn.config.datagram_receive_buffer_size.is_none() {
+            return None;
+        }
+
         // We use the conservative overhead bound for any packet number, reducing the budget by at
         // most 3 bytes, so that PN size fluctuations don't cause users sending maximum-size
         // datagrams to suffer avoidable packet loss.
